f4fe1e5894c55ad50f4f547906d09a8c9ef2b203
Sergei Banshchikov <s.o.banshchikov@gmail.com>
2021-10-17 22:34:50 +0200

    board position is fixed

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 import { flowControl } from "./flowControl.js";
 
-// const board = [
-//     ['V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B'],
-//     ['B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V'],
-//     ['V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B'],
-//     ['B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V'],
-//     ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V'],
-//     ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V'],
-//     ['V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W'],
-//     ['W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V'],
-//     ['V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W'],
-//     ['W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V']
-// ];
-
 const board = [
     ['V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B'],
     ['B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V'],
     ['V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B'],
-    ['B', 'V', 'B', 'V', 'B', 'V', 'V', 'V', 'B', 'V'],
-    ['V', 'V', 'V', 'V', 'V', 'B', 'V', 'B', 'V', 'V'],
+    ['B', 'V', 'B', 'V', 'B', 'V', 'B', 'V', 'B', 'V'],
     ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V'],
-    ['V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'V'],
+    ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V'],
+    ['V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W'],
     ['W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V'],
     ['V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W'],
     ['W', 'V', 'W', 'V', 'W', 'V', 'W', 'V', 'W', 'V']
@@ -44,7 +31,6 @@ for (let row = 0; row < board.length; row++) {
     for (let man = 0; man < board.length; man++) {
         let btn = document.createElement("button");
         btn.classList.add(board[row][man]);
-        if (row == 6 && man == 3) { btn.classList.add('Q'); board[row][man] += 'Q';}
         btn.setAttribute('data-y', row);
         btn.setAttribute('data-x', man);
         btn.onclick = function() {
